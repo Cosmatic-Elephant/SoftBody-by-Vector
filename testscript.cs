28ca2af4d5534b57bb84d686d6bac3dc4fd8681e
Cosmatic-Elephant <tjdwls530@gmail.com>
2024-01-15 15:56:16 +0900

    add testscript

---
+++ testscript.cs
@@ -0,0 +1,10 @@
+using system.Collections;
+using system.Collections.Generic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        public static void Main()
+    }
+}
